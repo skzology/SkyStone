f521c5c8d91687332ecf0c208c158aac1409264d
ripWaterSheep <joshuadherer@icloud.com>
2019-10-08 18:18:18 -0400

    Optimized imports

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveConstants.java
@@ -3,18 +3,27 @@ package org.firstinspires.ftc.teamcode;
 public class DriveConstants {
 
     /*
-    *
-    * USE INCHES
-    *
-    */
+     *
+     * All the robot's constants relating to its motion and measurements
+     *
+     */
 
-    private static final double WHEEL_RADIUS = 2;
-    private static final double WHEEL_CIRCUMFERENCE = WHEEL_RADIUS*2*Math.PI;
+    public static final double WHEEL_RADIUS = 2;
+    public static final double WHEEL_CIRCUMFERENCE = WHEEL_RADIUS*2*Math.PI;
 
-    private static final double WHEEL_BASE = 10; // length between front & back wheels
-    private static final double TRACK_WIDTH = 16; // width between left and right wheels
-    private static final double CIRCUMSCRIBED_DIAMETER = Math.sqrt(Math.pow(WHEEL_BASE, 2) + (Math.pow(TRACK_WIDTH, 2))); // Distance between centers of 2 diagonal wheels
-    private static final double CIRCUMSCRIBED_CIRCUMFERENCE = CIRCUMSCRIBED_DIAMETER*Math.PI;
+    // length between front & back wheels
+    public static final double WHEEL_BASE = 10;
+
+    // width between left and right wheels
+    public static final double TRACK_WIDTH = 16;
+
+    /*
+     * diameter of the circumcircle of the robot, really the distance between centers of 2 diagonal
+     * wheels (hypotenuse of wheelbase and track width)
+     */
+    public static final double CIRCUMDIAMETER = Math.hypot(WHEEL_BASE, TRACK_WIDTH);
+    //Circumference of circle
+    public static final double CIRCUMCIRCLE_CIRCUMFERENCE = CIRCUMDIAMETER*Math.PI;
 
     public static double inchesToTicks(int inches) {
         return (inches / WHEEL_CIRCUMFERENCE) * 360;
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Autonomous/SeekSkyStone.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Autonomous/SeekSkyStone.java
@@ -29,6 +29,7 @@
 
 package org.firstinspires.ftc.teamcode.OpModes.Autonomous;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -51,9 +52,9 @@ import java.util.List;
  * IMPORTANT: In order to use this OpMode, you need to obtain your own Vuforia license key as
  * is explained below.
  */
-@TeleOp(name = "Concept: TensorFlow Object Detection Webcam", group = "Concept")
-@Disabled
-public class ConceptTensorFlowObjectDetectionWebcam extends LinearOpMode {
+@Autonomous(name = "SeekSkyStone", group = "Autonomous")
+//@Disabled
+public class SeekSkyStone extends LinearOpMode {
     private static final String TFOD_MODEL_ASSET = "Skystone.tflite";
     private static final String LABEL_FIRST_ELEMENT = "Stone";
     private static final String LABEL_SECOND_ELEMENT = "Skystone";
@@ -167,4 +168,5 @@ public class ConceptTensorFlowObjectDetectionWebcam extends LinearOpMode {
        tfod = ClassFactory.getInstance().createTFObjectDetector(tfodParameters, vuforia);
        tfod.loadModelFromAsset(TFOD_MODEL_ASSET, LABEL_FIRST_ELEMENT, LABEL_SECOND_ELEMENT);
     }
+
 }
